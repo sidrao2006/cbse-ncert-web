3158d8a39020be29505b55e3843b44340a9ae7ab
Bhavya Maheshwari <you@example.com>
2021-08-20 20:27:39 +0530

    Add Portal and Promo site

---
+++ portal/Extra.js
@@ -0,0 +1,170 @@
+var sea1 = document.getElementById("sea1").value;
+var sea2 = document.getElementById("sea2").value;
+var sea3 = document.getElementById("sea3").value;
+if (sea1 != "" && (sea1 <= 5)) {
+    if (sea1 >= 0) {
+        document.getElementById("seagrade1").innerHTML = "E2";
+    }
+} else if (sea1 != "" && (sea1 <= 8)) {
+    if (sea1 >= 5.25) {
+        document.getElementById("seagrade1").innerHTML = "E1";
+    }
+} else if (sea1 != "" && (sea1 <= 10)) {
+    if (sea1 >= 8.25) {
+        document.getElementById("seagrade1").innerHTML = "D";
+    }
+} else if (sea1 != "" && (sea1 <= 12.5)) {
+    if (sea1 >= 10.25) {
+        document.getElementById("seagrade1").innerHTML = "C2";
+    }
+} else if (sea1 != "" && (sea1 <= 15)) {
+    if (sea1 >= 12.75) {
+        document.getElementById("seagrade1").innerHTML = "C1";
+    }
+} else if (sea1 != "" && (sea1 <= 17.5)) {
+    if (sea1 >= 15.25) {
+        document.getElementById("seagrade1").innerHTML = "B2";
+    }
+} else if (sea1 != "" && (sea1 <= 20)) {
+    if (sea1 >= 17.75) {
+        document.getElementById("seagrade1").innerHTML = "B1";
+    }
+} else if (sea1 != "" && (sea1 <= 22.5)) {
+    if (sea1 >= 20.25) {
+        document.getElementById("seagrade1").innerHTML = "A2";
+    }
+} else if (sea1 != "" && (sea1 <= 25)) {
+    if (sea1 >= 22.75) {
+        document.getElementById("seagrade1").innerHTML = "A1";
+    }
+} else {
+    document.getElementById("seagrade1").innerHTML = "";
+}
+
+if (sea2 != "" && (sea2 <= 5)) {
+    if (sea2 >= 0) {
+        document.getElementById("seagrade2").innerHTML = "E2";
+    }
+} else if (sea2 != "" && (sea2 <= 8)) {
+    if (sea2 >= 5.25) {
+        document.getElementById("seagrade2").innerHTML = "E1";
+    }
+} else if (sea2 != "" && (sea2 <= 10)) {
+    if (sea2 >= 8.25) {
+        document.getElementById("seagrade2").innerHTML = "D";
+    }
+} else if (sea2 != "" && (sea2 <= 12.5)) {
+    if (sea2 >= 10.25) {
+        document.getElementById("seagrade2").innerHTML = "C2";
+    }
+} else if (sea2 != "" && (sea2 <= 15)) {
+    if (sea2 >= 12.75) {
+        document.getElementById("seagrade2").innerHTML = "C1";
+    }
+} else if (sea2 != "" && (sea2 <= 17.5)) {
+    if (sea2 >= 15.25) {
+        document.getElementById("seagrade2").innerHTML = "B2";
+    }
+} else if (sea2 != "" && (sea2 <= 20)) {
+    if (sea2 >= 17.75) {
+        document.getElementById("seagrade2").innerHTML = "B1";
+    }
+} else if (sea2 != "" && (sea2 <= 22.5)) {
+    if (sea2 >= 20.25) {
+        document.getElementById("seagrade2").innerHTML = "A2";
+    }
+} else if (sea2 != "" && (sea2 <= 25)) {
+    if (sea2 >= 22.75) {
+        document.getElementById("seagrade2").innerHTML = "A1";
+    }
+} else {
+    document.getElementById("seagrade2").innerHTML = "";
+}
+
+if (sea3 != "" && (sea3 <= 5)) {
+    if (sea3 >= 0) {
+        document.getElementById("seagrade3").innerHTML = "E2";
+    }
+} else if (sea3 != "" && (sea3 <= 8)) {
+    if (sea3 >= 5.25) {
+        document.getElementById("seagrade3").innerHTML = "E1";
+    }
+} else if (sea3 != "" && (sea3 <= 10)) {
+    if (sea3 >= 8.25) {
+        document.getElementById("seagrade3").innerHTML = "D";
+    }
+} else if (sea3 != "" && (sea3 <= 12.5)) {
+    if (sea3 >= 10.25) {
+        document.getElementById("seagrade3").innerHTML = "C2";
+    }
+} else if (sea3 != "" && (sea3 <= 15)) {
+    if (sea3 >= 12.75) {
+        document.getElementById("seagrade3").innerHTML = "C1";
+    }
+} else if (sea3 != "" && (sea3 <= 17.5)) {
+    if (sea3 >= 15.25) {
+        document.getElementById("seagrade3").innerHTML = "B2";
+    }
+} else if (sea3 != "" && (sea3 <= 20)) {
+    if (sea3 >= 17.75) {
+        document.getElementById("seagrade3").innerHTML = "B1";
+    }
+} else if (sea3 != "" && (sea3 <= 22.5)) {
+    if (sea3 >= 20.25) {
+        document.getElementById("seagrade3").innerHTML = "A2";
+    }
+} else if (sea3 != "" && (sea3 <= 25)) {
+    if (sea3 >= 22.75) {
+        document.getElementById("seagrade3").innerHTML = "A1";
+    }
+} else {
+    document.getElementById("seagrade3").innerHTML = "";
+}
+
+
+function PTmarks() {
+    var pt1 = document.getElementById("pt1").value;
+    var pt2 = document.getElementById("pt1").value;
+    var pt3 = document.getElementById("pt3").value;
+
+    if (sea1 != "" && (sea1 <= 5)) {
+        if (sea1 >= 0) {
+            document.getElementById("seagrade1").innerHTML = "E2";
+        }
+    } else if (sea1 != "" && (sea1 <= 8)) {
+        if (sea1 >= 5.25) {
+            document.getElementById("seagrade1").innerHTML = "E1";
+        }
+    } else if (sea1 != "" && (sea1 <= 10)) {
+        if (sea1 >= 8.25) {
+            document.getElementById("seagrade1").innerHTML = "D";
+        }
+    } else if (sea1 != "" && (sea1 <= 12.5)) {
+        if (sea1 >= 10.25) {
+            document.getElementById("seagrade1").innerHTML = "C2";
+        }
+    } else if (sea1 != "" && (sea1 <= 15)) {
+        if (sea1 >= 12.75) {
+            document.getElementById("seagrade1").innerHTML = "C1";
+        }
+    } else if (sea1 != "" && (sea1 <= 17.5)) {
+        if (sea1 >= 15.25) {
+            document.getElementById("seagrade1").innerHTML = "B2";
+        }
+    } else if (sea1 != "" && (sea1 <= 20)) {
+        if (sea1 >= 17.75) {
+            document.getElementById("seagrade1").innerHTML = "B1";
+        }
+    } else if (sea1 != "" && (sea1 <= 22.5)) {
+        if (sea1 >= 20.25) {
+            document.getElementById("seagrade1").innerHTML = "A2";
+        }
+    } else if (sea1 != "" && (sea1 <= 25)) {
+        if (sea1 >= 22.75) {
+            document.getElementById("seagrade1").innerHTML = "A1";
+        }
+    } else {
+        document.getElementById("seagrade1").innerHTML = "";
+    }
+
+}
